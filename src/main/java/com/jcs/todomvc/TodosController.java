66c8bef171925a02a12640f7bbf27c27ff924bf3
jcsantosbr <senha123>
2014-08-06 01:13:25 -0700

    changing implementation of the controller

--- src/main/java/com/jcs/todomvc/TodosController.java
+++ src/main/java/com/jcs/todomvc/TodosController.java
@@ -3,6 +3,7 @@ package com.jcs.todomvc;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,9 +12,11 @@ import static java.util.Arrays.asList;
 @RestController
 public class TodosController {
 
+    private List<Todo> todos = new ArrayList<Todo>();
+
     @RequestMapping("/todos")
     public List<Todo> listAll() {
-        return asList(new Todo("one todo"), new Todo("two todo"));
+        return todos;
     }
 
 }
