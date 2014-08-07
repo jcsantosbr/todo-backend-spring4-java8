0ea76740e18593eeda2addfe9fe2a060c633029a
jcsantosbr <senha123>
2014-08-07 00:53:25 -0700

    add post operation to controller

--- src/main/java/com/jcs/todomvc/TodosController.java
+++ src/main/java/com/jcs/todomvc/TodosController.java
@@ -1,8 +1,11 @@
 package com.jcs.todomvc;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,13 +13,20 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 @RestController
+
 public class TodosController {
 
     private List<Todo> todos = new ArrayList<Todo>();
 
-    @RequestMapping("/todos")
+    @RequestMapping(value = "/todos", method = RequestMethod.GET)
     public List<Todo> listAll() {
         return todos;
+    }
+
+    @RequestMapping(value = "/todos", method = RequestMethod.POST)
+    public Todo saveTodo(Todo todo) {
+        todos.add(todo);
+        return todo;
     }
 
 }
