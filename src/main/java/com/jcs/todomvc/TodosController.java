aedafe71e8dfaa6139574a763f5d9883b1c14955
jcsantosbr <senha123>
2014-08-12 00:03:27 -0700

    changing method to list all to return json with url for each todo

--- src/main/java/com/jcs/todomvc/TodosController.java
+++ src/main/java/com/jcs/todomvc/TodosController.java
@@ -5,51 +5,60 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 @RequestMapping(value = "/todos")
 public class TodosController {
 
-    private List<Todo> todos = new ArrayList<Todo>();
-    private TodosController thisController = methodOn(this.getClass());
+    private Set<Todo> todos = new HashSet<>();
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Todo> listAll() {
-        return todos;
+    @RequestMapping(method = GET)
+    public HttpEntity<Collection<ResourceWithUrl>> listAll() {
+        List<ResourceWithUrl> resourceWithUrls = todos.stream().map(todo -> toResource(todo)).collect(Collectors.toList());
+        return new ResponseEntity<>(resourceWithUrls, OK);
     }
 
-    @RequestMapping(value = "/{todo-title}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{todo-title}", method = GET)
     public HttpEntity<ResourceWithUrl> getTodo(@PathVariable("todo-title") String title ) {
 
         Optional<Todo> todoOptional = todos.stream().filter(todo -> todo.getTitle().equals(title)).findFirst();
 
         if (!todoOptional.isPresent())
-            return new ResponseEntity<ResourceWithUrl>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(NOT_FOUND);
 
-        return respondeWithResource(todoOptional.get(), HttpStatus.OK);
+        return respondeWithResource(todoOptional.get(), OK);
     }
 
     private String getHref(Todo todo) {
-        return linkTo( thisController.getTodo(todo.getTitle()) ).withSelfRel().getHref();
+        return linkTo( methodOn(this.getClass()).getTodo(todo.getTitle()) ).withSelfRel().getHref();
     }
 
-    @RequestMapping(method = RequestMethod.POST,  headers = {"Content-type=application/json"})
+    @RequestMapping(method = POST,  headers = {"Content-type=application/json"})
     public HttpEntity<ResourceWithUrl> saveTodo(@RequestBody Todo todo) {
         todos.add(todo);
 
-        return respondeWithResource(todo, HttpStatus.CREATED);
+        return respondeWithResource(todo, CREATED);
     }
 
     private HttpEntity<ResourceWithUrl> respondeWithResource(Todo todo, HttpStatus statusCode) {
-        ResourceWithUrl resourceWithUrl = new ResourceWithUrl(todo, getHref(todo));
+        ResourceWithUrl resourceWithUrl = toResource(todo);
 
         return new ResponseEntity<>(resourceWithUrl, statusCode);
+    }
+
+    private ResourceWithUrl toResource(Todo todo) {
+        return new ResourceWithUrl(todo, getHref(todo));
     }
 
     @RequestMapping(method = RequestMethod.DELETE)
