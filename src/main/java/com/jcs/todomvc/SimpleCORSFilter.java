588fb881d6548c22265a296093b255b5139f4163
jcsantosbr <senha123>
2014-08-12 00:48:12 -0700

    adding verb PATCH to filter

--- src/main/java/com/jcs/todomvc/SimpleCORSFilter.java
+++ src/main/java/com/jcs/todomvc/SimpleCORSFilter.java
@@ -17,7 +17,7 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
         chain.doFilter(req, res);
